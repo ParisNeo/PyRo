1e96462f79d12ce5d1d30d7996b6f585a59e7949
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2025-03-04 12:38:35 +0100

    done

--- src/python.rs
+++ src/python.rs
@@ -7,6 +7,7 @@ use select::predicate::Attr;
 use select::predicate::Class;
 use std::error::Error;
 
+
 #[derive(Deserialize, Debug)]
 pub struct PythonVersion {
     pub version: String,
@@ -32,37 +33,43 @@ impl PythonManager {
 
     async fn fetch_python_versions() -> Result<Vec<PythonVersion>, PyroError> {
         let url = "https://www.python.org/downloads/";
-        let response = reqwest::get(url).await?;
+    
+        // Build a client with a custom User-Agent header
+        let client = reqwest::Client::builder()
+            .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36")
+            .build()?;
+    
+        let response = client.get(url).send().await?;
         let body = response.text().await?;
-
+    
         let document = Html::parse_document(&body);
-
+    
         // Selector for the container that holds the version list
         let container_selector = Selector::parse("div.download-list-widget").unwrap();
         let container = document.select(&container_selector).next().ok_or_else(|| {
             PyroError::Custom("Could not find the version container on the page.".to_string())
         })?;
-
+    
         // Selector for the nested `ol` tag within the container
         let ol_selector = Selector::parse("ol.list-row-container.menu").unwrap();
         let ol = container.select(&ol_selector).next().ok_or_else(|| {
             PyroError::Custom("Could not find the version list within the container.".to_string())
         })?;
-
+    
         // Selector for the `li` tags within the `ol`
         let li_selector = Selector::parse("li").unwrap();
         let version_selector = Selector::parse("span.release-number").unwrap();
-
+    
         let mut versions = Vec::new();
-
+    
         for li in ol.select(&li_selector) {
             if let Some(version_span) = li.select(&version_selector).next() {
                 let version = version_span.text().collect::<String>().trim().to_string();
-                let url = format!("https://www.python.org/downloads/release/{}/", version);
-                versions.push(PythonVersion { version, url });
+                let version_url = format!("https://www.python.org/downloads/release/{}/", version);
+                versions.push(PythonVersion { version, url: version_url });
             }
         }
-
+    
         Ok(versions)
     }
     
